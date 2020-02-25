389eafeb9c8ae9553b963beba960123b3e43c431
Magomed Shamaev <ms77grz@gmail.com>
2020-02-25 11:49:28 +0300

    final steps are done

--- crimemap.py
+++ crimemap.py
@@ -1,59 +1,56 @@
+import datetime
+import dateparser
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
     from dbhelper import DBHelper
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
+import string
 
 
 app = Flask(__name__)
+app.secret_key = dbconfig.secret_key
 DB = DBHelper()
+categories = ['mugging', 'break-in', 'robbery', 'burglary']
 
 
-# @app.route("/")
-# def home():
-#     try:
-#         data = DB.get_all_inputs()
-#     except Exception as e:
-#         print(e)
-#         data = None
-#     return render_template("home.html", data=data)
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return ''.join(list(filter(lambda x: x in whitelist, userinput)))
 
 
 @app.route("/")
 def home():
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
-
-
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return redirect(url_for("home"))
-
-
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return redirect(url_for("home"))
+    return render_template("home.html", crimes=crimes, categories=categories)
 
 
 @app.route("/submitcrime", methods=["POST"])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return redirect(url_for("home"))
+    date = format_date(request.form.get("date"))
+    if not date:
+        flash("Invalid date. Please use yyyy-mm-dd format")
+        return redirect(url_for("home"))
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return redirect(url_for("home"))
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return redirect(url_for("home"))
 
